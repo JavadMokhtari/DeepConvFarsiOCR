ecbb0c3c8fcf9f4858d56332064dbf66603e36d3
javad-mokhtari <javad.mktri@gmail.com>
2022-12-12 03:47:07 +0330

    Change func to beter usage

--- Convert_to_PNG.py
+++ Convert_to_PNG.py
@@ -1,30 +1,41 @@
 import os
+from random import randint
+from sys import argv
 from PIL import Image
 
 
 # Function for convert images format to PNG
-def ConvertToPNG(images_path, new_path):
+def ConvertToPNG(directories):
+    if '--images_dir' in directories:
+        images_dir = directories[directories.index('--images_dir')+1]
+    else:
+        print("Enter images data directory!")
+        return
+    if '--dir_to_save' in directories:
+        dir_to_save = directories[directories.index('--dir_to_save')+1]
+    else:
+        dir_to_save = 'Converted-Images-' + str(randint(0, 100))
+
     # Create a new directory for save new images
-    if not os.path.exists(new_path):
-        os.makedirs(new_path)
+    if not os.path.exists(dir_to_save):
+        os.makedirs(dir_to_save)
     # Get all existing names of images
-    fnames = os.listdir(images_path)
+    fnames = os.listdir(images_dir)
     # Save images with new format .png in each class folder
     for fname in fnames:
         if '.bmp' in fname:
             # Split the class number from image file
             class_num = fname.split('.bmp')[0].split('_')[1]
-            class_path = os.path.join(new_path, class_num)
+            class_path = os.path.join(dir_to_save, class_num)
             if not os.path.exists(class_path):
                 os.makedirs(class_path)
             # Save image in its class path
             try:
                 path_storage = os.path.join(class_path, fname[:-4] + '.png')
-                img_path = os.path.join(images_path, fname)
+                img_path = os.path.join(images_dir, fname)
                 Image.open(img_path).save(path_storage)
             except:
                 print('Error occurred while saving')
 
 if __name__=='__main__':
-    ConvertToPNG('PDB-Test', 'Test-Data')
-    ConvertToPNG('PDB-Train', 'Train-Data')
+    ConvertToPNG(argv[1:])
